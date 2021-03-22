ef957887ce13d4c2e157826c3728de683ab35fd5
Taksungjin <tak3798@naver.com>
2021-03-22 19:09:20 +0900

    0322

---
+++ be/demo/src/main/java/com/example/demo/uss/service/AdminServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.uss.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+	// private final AdminRepository repo;
+	
+	
+
+}
